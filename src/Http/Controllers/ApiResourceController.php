2c5ade0f55dc2cbd328d9a5bd48f022fc226de35
Syed Abdur Rehman Kazmi <sydabdrehman@gmail.com>
2018-08-28 18:31:42 +0000

    ApiResourceController.php edited online with Bitbucket

--- src/Http/Controllers/ApiResourceController.php
+++ src/Http/Controllers/ApiResourceController.php
@@ -6,10 +6,12 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 
+
 abstract class ApiResourceController extends Controller
 {
     public $_repository;
-    const   PER_PAGE = 10;
+    
+    const   PER_PAGE = 25;
 
     public function __constructor($repository)
     {
@@ -21,18 +23,24 @@ abstract class ApiResourceController extends Controller
     public function index(Request $request)
     {
 
-        
         $rules = $this->rules(__FUNCTION__);
         $input = $this->input(__FUNCTION__);
 
         $this->validate($request, $rules);
         
         $per_page = self::PER_PAGE ? self::PER_PAGE : config('app.per_page');
-        $pagination = !empty($input['pagination']) && $input['pagination'] == 'true' ? true : false; 
+
+        $pagination = !empty($input['pagination']) ? $input['pagination'] : false; 
 
         $data = $this->_repository->findByAll($pagination, $per_page, $input);
 
-        $output = ['response' => ['data' => $data['data'], 'pagination' => !empty($data['pagination']) ? $data['pagination'] : false   , 'message' => 'Success']];
+        $output = [
+            'response' => [
+                'data' => $data['data'],
+                'pagination' => !empty($data['pagination']) ? $data['pagination'] : false,
+                'message' => $this->response_messages(__FUNCTION__),
+            ]
+        ];
 
         // HTTP_OK = 200;
 
@@ -44,17 +52,16 @@ abstract class ApiResourceController extends Controller
     //Get single record
     public function show(Request $request,$id)
     {
-
         $request->request->add(['id' => $id]);
 
         $rules = $this->rules(__FUNCTION__);
         $input = $this->input(__FUNCTION__);
-
+        
         $this->validate($request, $rules);
 
-        $data = $this->_repository->findById($input['id'], $refresh = false, $details = false, $encode = true);
+        $data = $this->_repository->findById($input['id'], $refresh = false, $input, $encode = true);
 
-        $output = ['response' => ['data' => $data, 'message' => 'Success']];
+        $output = ['response' => ['data' => $data]];
 
         // HTTP_OK = 200;
 
@@ -65,17 +72,18 @@ abstract class ApiResourceController extends Controller
 
     //Create single record
     public function store(Request $request)
-    {
-
-
+    {   
         $rules = $this->rules(__FUNCTION__);
         $input = $this->input(__FUNCTION__);
+        
+        $messages = $this->messages(__FUNCTION__);
 
-        $this->validate($request, $rules);
+        $this->validate($request, $rules, $messages);
+        
         $data = $this->_repository->create($input);
 
-        $output = ['response' => ['data' => $data, 'message' => 'Record added successfully']];
-
+        $output = ['response' => ['data' => $data, 'message' => $this->response_messages(__FUNCTION__)]];
+        
         // HTTP_OK = 200;
 
         return response()->json($output, Response::HTTP_OK);
@@ -85,15 +93,17 @@ abstract class ApiResourceController extends Controller
     //Update single record
     public function update(Request $request, $id)
     {   
-
         $request->request->add(['id' => $id]);
+        
         $input = $this->input(__FUNCTION__);
         $rules = $this->rules(__FUNCTION__);
+        
+        $messages = $this->messages(__FUNCTION__);
 
-        $this->validate($request, $rules);
+        $this->validate($request, $rules, $messages);
+
         $data = $this->_repository->update($input);
-
-        $output = ['response' => ['data' => $data, 'message' => 'Record updated successfully']];
+        $output = ['response' => ['data' => $data, 'message' => $this->response_messages(__FUNCTION__)]];
 
         // HTTP_OK = 200;
 
@@ -106,14 +116,17 @@ abstract class ApiResourceController extends Controller
     public function destroy(Request $request, $id)
     {
         $request->request->add(['id' => $id]);
+
         $rules = $this->rules(__FUNCTION__);
         $input = $this->input(__FUNCTION__);
 
-        $this->validate($request, $rules);
+        $messages = $this->messages(__FUNCTION__);
+
+        $this->validate($request, $rules, $messages);
 
         $data = $this->_repository->deleteById($input['id']);
 
-        $output = ['response' => ['data' => $data, 'message' => 'Success']];
+        $output = ['response' => ['data' => $data, 'message' => $this->response_messages(__FUNCTION__)]];
 
         // HTTP_OK = 200;
 
@@ -131,6 +144,26 @@ abstract class ApiResourceController extends Controller
     public function input($value = '')
     {
         return [];
+    }
+
+    public function messages($value = '')
+    {
+        $messages = [
+
+        ];
+        
+        return !empty($messages) ? $messages : [];
+    }
+
+    public function response_messages($value = '')
+    {
+        $messages = [
+            'store' => 'Record created successfully.',
+            'update' => 'Record updated successfully.',
+            'destroy' => 'Record deleted successfully.',
+        ];
+        
+        return !empty($messages[$value]) ? $messages[$value] : 'Success.';
     }
 
 }
